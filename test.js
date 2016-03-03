ef66fb2ae7921aa5d9353c97c75604b8a5e211ae
Max Donchenko <wereya2@gmail.com>
2016-03-03 16:20:50 +0200

    Add more tests

--- test.js
+++ test.js
@@ -30,6 +30,51 @@ describe("Make a war", function() {
 
   	});
 
+  	it("W: 8600 hp, A: 10000+3600+5000 hp [at house Stark]", function() {
+    	assert.equal(gameOfThrones([
+		    						"Wildling:860",
+		    						"Stark:out:2000",
+		    						"Arryn:in:300",
+		    						"Greyjoy:in:300",
+		    						"Lannister:500",
+		    						"Stannis:out:0",
+		    						"Tyrell:200",
+		    						"Martell:out:300"
+    								]), "Wildlings were stopped at house Stark!"
+    				);
+
+  	});
+
+  	it("W: 8600 hp, A: 3600+5000+2500 hp [at house Lannister]", function() {
+    	assert.equal(gameOfThrones([
+		    						"Wildling:860",
+		    						"Stark:out:0",
+		    						"Arryn:in:300",
+		    						"Greyjoy:in:300",
+		    						"Lannister:1000",
+		    						"Stannis:out:0",
+		    						"Tyrell:200",
+		    						"Martell:out:300"
+    								]), "Wildlings were stopped at house Lannister!"
+    				);
+
+  	});
+
+  	it("W: 8600 hp, A: 3600+2500+2500+1500 hp [at house Tyrell]", function() {
+    	assert.equal(gameOfThrones([
+		    						"Wildling:860",
+		    						"Stark:out:0",
+		    						"Arryn:in:300",
+		    						"Greyjoy:in:300",
+		    						"Lannister:500",
+		    						"Stannis:out:0",
+		    						"Tyrell:500",
+		    						"Martell:out:300"
+    								]), "Wildlings were stopped at house Tyrell!"
+    				);
+
+  	});
+
   	it("W: 3000 hp, A: 3600 hp [Alliance won]", function() {
     	assert.equal(gameOfThrones([
 		    						"Wildling:300",
